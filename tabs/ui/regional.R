ea147cdf93af7a4cc61e526c69f798bd76754380
branmora <59512596+branmora@users.noreply.github.com>
2021-04-26 08:50:34 -0500

    update data 25.04

--- tabs/ui/regional.R
+++ tabs/ui/regional.R
@@ -14,9 +14,9 @@ regional <- tabPanel(title = "Nivel Regional",
                    # downloadButton("reporte", "Generar reporte"),
                    fluidRow(
                    box(width = 12, 
-                   valueBox(h2(strong("35,700")), "Casos totales 2021", icon = icon("virus", "fa-2x")),
-                   valueBox(h2(strong("194,642")), "Pruebas procesadas 2021 (moleculares, rápidas y antigénicas)", icon = icon("vial", "fa-2x")),
-                   valueBox(h2(strong("1,488")), "Total Fallecidos 2021", icon = icon("skull-crossbones","fa-2x")))
+                   valueBox(h2(strong("38,907")), "Casos totales 2021", icon = icon("virus", "fa-2x")),
+                   valueBox(h2(strong("211,188")), "Pruebas procesadas 2021 (moleculares, rápidas y antigénicas)", icon = icon("vial", "fa-2x")),
+                   valueBox(h2(strong("1,624")), "Total Fallecidos 2021", icon = icon("skull-crossbones","fa-2x")))
                  ),
 
                 # Fila 1
